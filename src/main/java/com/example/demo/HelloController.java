3a6a8fe9ab77a709e656fbd9bf73069b60aeecfc
450128 <d.somsen@student.fontys.nl>
2022-03-10 14:26:50 +0100

    CORS fix

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+@CrossOrigin(origins = "https://master.d3m2jwhbvk1whf.amplifyapp.com")
 @RestController
 public class HelloController {
 
